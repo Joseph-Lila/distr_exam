1d70fad72ca959187104d01c6e4d05b162eab3ff
Artur Prakapenka <80418302+Joseph-Lila@users.noreply.github.com>
2022-12-27 17:47:58 +0300

    gui connection screen view

--- src/entrypoints/server/main.py
+++ src/entrypoints/server/main.py
@@ -6,8 +6,15 @@ from src.entrypoints.server.screens import ScreenGenerator
 class ServerApp(MDApp):
     title = 'Server'
 
-    def build(self):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.load_all_kv_files(self.directory)
+
+    def build(self):
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "BlueGray"
+        self.theme_cls.primary_hue = "700"
+        self.theme_cls.material_style = "M3"
         return ScreenGenerator().build_app_view()
 
 
